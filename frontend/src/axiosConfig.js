c81e26a6fee4c2a0a6f219ec47922cfd61325c73
Tejasgupta74 <157612264+Tejasgupta74@users.noreply.github.com>
2026-05-20 17:24:28 +0530

    3

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://task-manager-89vu.onrender.com'
 });
 
 // Add token to all requests
